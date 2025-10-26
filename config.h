009e315ece23070bf30e581ed345066edc055188
Layne Wedderburn <layne@wedderburn.co.za>
2025-10-26 15:59:46 +0000

    Configure fonts
    Use Roboto Mono as primary.
    Add Noto Color Emoji as font2.

--- config.h
+++ config.h
@@ -5,9 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "DejaVu Sans Mono:pixelsize=18:antialias=true:autohint=true";
+ static char *font = "Roboto Mono:pixelsize=18:antialias=true:autohint=true";
+
 /* Spare fonts */
 static char *font2[] = {
+	"Noto Color Emoji:size=14"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
